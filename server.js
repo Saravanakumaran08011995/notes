4d02ffb611e52a3dd5aea982b981532661a3326f
Saravanakumaran08011995 <saravanakumaran8195@gmail.com>
2023-07-27 21:59:44 +0530

    vercel

--- server.js
+++ server.js
@@ -1,41 +1,39 @@
-require('dotenv').config()
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const userRouter = require('./routes/userRouter')
-const nodeRouter = require('./routes/nodeRouter')
-const path = require('path')
-
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const userRouter = require('./routes/userRouter');
+const nodeRouter = require('./routes/nodeRouter');
+const path = require('path');
 
 const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-//Routes
-app.use('/user',userRouter)
-app.use('/api/notes',nodeRouter)
+// Routes
+app.use('/user', userRouter);
+app.use('/api/notes', nodeRouter);
 
+// Connection
+const URI = process.env.DATABASE;
+mongoose.connect(URI, err => {
+    if (err) throw err;
+    console.log('Connected to MongoDB');
+});
 
-//connection
-const URI = process.env.DATABASE
-mongoose.connect(URI,err=>{
-    if(err) throw err;
-    console.log('connecte to mongodb')
+// Route for /
+app.get('/', (req, res) => {
+    res.send('I am working');
+});
 
-})
-
-
-if(process.env.NODE_ENV === 'production'){
+if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
-    app.get('*', (req, res) =>{
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
     });
 }
 
-
-//listen server
-const PORT = process.env.PORT
-app.listen(PORT,()=>console.log(`server is listining on port ${PORT}` ))
-
-
+// Listen to the server
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
